511b2005aa8b50a0459e79e2eaa179c0fbcb439c
Giang Van <noname7652259@gmail.com>
2020-02-23 02:58:02 +0700

    register

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,17 +1,60 @@
 const ACCOUNT = require("../models/accounts");
+const RESPONSE_STATUS = require("../../system/response").status;
 
 module.exports = (res, fields, files) => {
-	console.log(fields);
-	// const USERNAME = req.body.username;
-	// const PWD = req.body.pwd;
-	// const ID = req.body.id;
-	// const ROLE = req.body.role;
-	// if (USERNAME && PWD && ID && ROLE) {
-	// 	let account = ACCOUNT.findById(ID);
-
-	// } else {
-	// 	RESULT.describe = "missing some account info";
-	// }
-
-	return res;
+	const USERNAME = fields.username;
+	const PWD = fields.pwd;
+	const ID = fields.id;
+	const ROLE = fields.role;
+	if (USERNAME && PWD && ID && ROLE) {
+		ACCOUNT.findById(ID).exec((error, doc) => {
+			if (error) {
+				res.describe = error.message;
+				res.end();
+			} else {
+				if (doc) {
+					res.data = doc;
+					res.describe = "ID";
+					res.end();
+				} else {
+					ACCOUNT.find({ username: USERNAME }, (error, doc) => {
+						if (error) {
+							res.describe = error.message;
+							res.end();
+						} else {
+							if (doc.length) {
+								res.data = doc;
+								res.describe = "USERNAME";
+								res.end();
+							} else {
+								ACCOUNT.create(
+									{
+										_id: ID,
+										username: USERNAME,
+										pwd: PWD,
+										role: ROLE
+									},
+									(error, doc) => {
+										if (error) {
+											res.describe = error.message;
+											res.end();
+										} else {
+											res.status =
+												RESPONSE_STATUS.SUCCESSFUL;
+											res.data = doc;
+											res.end();
+										}
+									}
+								);
+							}
+							0;
+						}
+					});
+				}
+			}
+		});
+	} else {
+		res.describe = "missing some account info";
+		res.end();
+	}
 };
