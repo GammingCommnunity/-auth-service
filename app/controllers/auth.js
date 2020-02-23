26c93ec12d4153a0838977ecd053bf75274e6327
Giang Van <noname7652259@gmail.com>
2020-02-23 19:28:40 +0700

    [auth] Completed

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,18 +1,21 @@
-const JWT = require('../helpers/jwt');
+const JWT = require("../helpers/jwt");
+const LOG = require("../../system/log");
+const RESPONSE_STATUS = require("../../config/response_status");
 
-module.exports = (res, fields, files) => {
+module.exports = (req, res, fields, files) => {
 	const TOKEN = req.headers["token"];
-	if(TOKEN){
+	if (TOKEN) {
 		const DECODED = JWT.decode(TOKEN);
-		if(DECODED){
-			res.result = true;
-			console.log(DECODED);
+		if (DECODED) {
+			res.status = RESPONSE_STATUS.SUCCESSFUL;
 		} else {
-			res.describe = 'wrong token';
+			res.describe = "wrong token";
+			LOG.writeRequest(req, fields, files, res.describe);
 		}
 	} else {
-		res.describe = 'missing the token';
+		res.describe = "missing the token";
+		LOG.writeRequest(req, fields, files, res.describe);
 	}
 
-	return res;
+	res.end();
 };
