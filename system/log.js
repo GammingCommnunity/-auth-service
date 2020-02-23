7cf14b5ac3048d78160272f988bea66769ee92fc
Giang Van <noname7652259@gmail.com>
2020-02-23 19:03:29 +0700

    [update framework] add error_handle for log feature

--- system/log.js
+++ system/log.js
@@ -3,6 +3,17 @@ const LOCALE = require("../config/app").LOCALE;
 const WRITE_LOG = require("../config/app").WRITE_LOG;
 const CONSOLE_COLOR = require("./consolelog_color");
 
+const LOG_ERROR_HANDLE = notErrorCallback => {
+	return error => {
+		if (error) {
+			CONSOLE_COLOR.red("Log writing error:\n");
+			console.log(error);
+		} else {
+			notErrorCallback();
+		}
+	};
+};
+
 exports.writeRequest = (
 	req,
 	formData = {},
@@ -11,7 +22,7 @@ exports.writeRequest = (
 	displayOnConsole = false
 ) => {
 	if (WRITE_LOG === undefined || WRITE_LOG) {
-		const DATE_NOW = new Date().toLocaleString(LOCALE);
+		const DATE_NOW = new Date().toLocaleString(LOCALE ? LOCALE : "vi-VN");
 		const DATA = {
 			header: req.headers,
 			form_data: formData,
@@ -22,32 +33,36 @@ exports.writeRequest = (
 			`[${DATE_NOW}] ${req.method} - ${req.url}` +
 			`\n${JSON.stringify(DATA, null, 4)}\n\n\n`;
 
-		FS.appendFile("log.txt", CONTENT, error => {
-			if (error) {
-				CONSOLE_COLOR.red("Log writing error:\n");
-				console.log(error);
-			} else if (displayOnConsole) {
-				//display a little bit of request info
-				CONSOLE_COLOR.green(`[${DATE_NOW}] ${req.method} - ${req.url}`);
-			}
-		});
+		FS.appendFile(
+			"log.txt",
+			CONTENT,
+			LOG_ERROR_HANDLE(() => {
+				if (displayOnConsole) {
+					//display a little bit of request info
+					CONSOLE_COLOR.green(
+						`[${DATE_NOW}] ${req.method} - ${req.url}`
+					);
+				}
+			})
+		);
 	}
 };
 
 exports.write = (text, displayOnConsole = false) => {
 	if (text) {
-		const DATE_NOW = new Date().toLocaleString(LOCALE);
+		const DATE_NOW = new Date().toLocaleString(LOCALE ? LOCALE : "vi-VN");
 		const CONTENT = `[${DATE_NOW}] message:\n${text}`;
 
-		FS.appendFile("log.txt", CONTENT, error => {
-			if (error) {
-				CONSOLE_COLOR.red("Log writing error:\n");
-				console.log(error);
-			} else if (displayOnConsole) {
-				//display a little bit of request info
-				CONSOLE_COLOR.green(`[${DATE_NOW}] message:\n`);
-				console.log(text);
-			}
-		});
+		FS.appendFile(
+			"log.txt",
+			CONTENT,
+			LOG_ERROR_HANDLE(() => {
+				if (displayOnConsole) {
+					//display a little bit of request info
+					CONSOLE_COLOR.green(`[${DATE_NOW}] message:\n`);
+					console.log(text);
+				}
+			})
+		);
 	}
 };
